c89ed0835f179749ef235ba416e4cd5ddb424ce3
JuFML <julianafernandezlima@hotmail.com>
2024-05-22 20:32:28 +0200

    ✅ request to get questions and render then

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 const App = () => {
-  return <h1>Boilerplate Vite + React</h1>;
+  const [questions, setQuestions] = useState([])
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  console.log(questions[currentQuestion])
+  
+  useEffect(() => {
+    fetch("http://localhost:5173/src/videogame-questions.json")
+      .then(response => response.json())
+      .then(data => setQuestions(data))
+  },[])
+
+  
+
+  return (
+    <div className="app">
+      <div className="main">
+        <h4>{questions[currentQuestion]?.question}</h4>
+        <ul className="options">
+          {questions[currentQuestion]?.options.map((option, index) => (
+          <li key={index} className="btn btn-option">{option}</li>))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default App;
