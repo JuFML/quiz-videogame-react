7d47eec29bba904a5c8f50605dabeea8ade5418d
JuFML <julianafernandezlima@hotmail.com>
2024-06-27 15:49:04 +0200

    ✅' implements timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,71 @@
 import { useReducer, useEffect } from "react";
 
-// const reducer = (state, action) => ({
-//   set_questions: {...state, questions: action.questions},
-//   set_currentQuestion: {...state, currentQuestion: action.currentQuestion},
-//   set_userScore: { ...state, userScore: action.userScore ? state.userScore + action.userScore : state.userScore},
-//   reset_userScore: { ...state, userScore: 0},
-//   set_clickedAnswer: {...state, clickedAnswer: action.clickedAnswer },
-//   set_countdown: {...state, countdown: action.countdown},
-//   set_appStatus: {...state, appStatus: action.appStatus}
-// })[action.type] || state
+const secondsPerQuestion = 30
 
   const reducer = (state, action) => {
 
     if(action.type === "set_questions") {
-      return {...state, questions: action.questions}
+      return {
+        ...state, 
+        questions: action.questions
+      }
     }
 
     if(action.type === "clicked_next_answer") {
-      return {...state, clickedAnswer: null, appStatus: action.isTheLastQuestion ? "finished" : state.appStatus, currentQuestion: action.isTheLastQuestion ? null : state.currentQuestion + 1}
+      return {
+        ...state, 
+        clickedAnswer: null, 
+        appStatus: action.isTheLastQuestion ? "finished" : state.appStatus, 
+        currentQuestion: action.isTheLastQuestion ? null : state.currentQuestion + 1, 
+        seconds: action.isTheLastQuestion ? null : state.seconds
+      }
     }
 
     if(action.type === "clicked_restart") {
-      return {...state, currentQuestion: null, userScore: action.userScore ? state.userScore + action.userScore : state.userScore, appStatus: "ready"}
+      return {
+        ...state, 
+        currentQuestion: null, 
+        userScore: action.userScore ? state.userScore + action.userScore : state.userScore, 
+        appStatus: "ready"
+      }
     }
 
     if(action.type === "clicked_start") {
-      return {...state, appStatus: "active", currentQuestion: 0}
+      return {
+        ...state, 
+        appStatus: "active", 
+        currentQuestion: 0, 
+        seconds: secondsPerQuestion * state.questions.length
+      }
     }
 
     if(action.type === "clicked_answer") {
-      return {...state, clickedAnswer: action.clickedAnswer, userScore: action.rightOption === action.clickedAnswer ? state?.questions[state.currentQuestion]?.points : 0}
+      return {
+        ...state, 
+        clickedAnswer: action.clickedAnswer, 
+        userScore: action.rightOption === action.clickedAnswer ? state?.questions[state.currentQuestion]?.points : 0
+      }
+    }
+
+    if(action.type === "tick") {
+      return {
+        ...state, 
+        seconds: state.seconds === 0 ? null : state.seconds - 1,
+        appStatus: state.seconds === 0 ? "finished" : state.appStatus
+      }
     }
 
     return state
   }
 
-const initialState = {questions: [], currentQuestion: null, clickedAnswer: null, userScore: 0, countdown: 50, appStatus: "ready"}
+const initialState = {questions: [], currentQuestion: null, clickedAnswer: null, userScore: 0, countdown: 50, appStatus: "ready", seconds: null}
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState)  
   let rightOption = state?.questions[state?.currentQuestion]?.correctOption 
   let isTheLastQuestion = state?.currentQuestion === state?.questions?.length - 1
+  const minutes = Math.floor(state.seconds/60)
+  const seconds = state.seconds % 60
 
   useEffect(() => {
     fetch("http://localhost:5173/src/videogame-questions.json")
@@ -49,47 +74,30 @@ const App = () => {
       .catch(error => alert(error.message))
   },[])
 
-  // useEffect(() => {
-    // let idIntervalo
-    // if(state.currentQuestion !== null) {
-      // const idIntervalo = setInterval(
-      //   dispatch({type: "set_countdown", countdown: state?.countdown > 0 ? state.countdown - 1 : 0})
-      //   , 10000)      
-    // }
-  //   return () => clearInterval(idIntervalo)
-    
-  // }, [state?.countdown])
+  useEffect(() => {
+    if(state.seconds === null) {
+      return
+    }
 
-  // const checkTheAnswer = (answer) => {
-  //   answer === rightOption
-  // }
+    const id = setTimeout(() => dispatch({type: "tick"}), 1000)
+    return () => clearTimeout(id)
+  }, [state.seconds])
 
   const handleClickAnswer = (answer) => {
     console.log(answer)
     dispatch({type: "clicked_answer", clickedAnswer: answer, rightOption})
-    // dispatch({type: "set_clickedAnswer", clickedAnswer: answer})
-    // dispatch({type: "set_userScore", userScore: rightOption === answer ? state?.questions[state.currentQuestion]?.points : 0})
-    // checkTheAnswer(answer)
   }
 
   const handleClickNext = () => {
     dispatch({type: "clicked_next_answer", isTheLastQuestion})
-    // dispatch({type: "set_clickedAnswer", clickedAnswer: null})
-    // dispatch({type: "set_appStatus", appStatus: isTheLastQuestion ? "finished" : state.appStatus})
-    // dispatch({type: "set_currentQuestion", currentQuestion: isTheLastQuestion ? null : state.currentQuestion + 1})
   }
 
   const handleClickRestart = () => {
     dispatch({type: "clicked_restart"})
-    // dispatch({type: "set_currentQuestion", currentQuestion: null })
-    // dispatch({type: "reset_userScore"})
-    // dispatch({type: "set_appStatus", appStatus: "ready"})
   }
 
   const handleClickStart = () => {
     dispatch({type: "clicked_start"})
-    // dispatch({type: "set_appStatus", appStatus: "active"})
-    // dispatch({type: "set_currentQuestion", currentQuestion: 0})
   }
 
   return (
@@ -124,7 +132,7 @@ const App = () => {
               </ul>
             </div>
             <div>
-              <div className="timer">00:{state.countdown}</div>
+              <div className="timer">{minutes < 10 ? `0${minutes}`: minutes}:{seconds < 10 ? `0${seconds}`: seconds}</div>
               { state.clickedAnswer != null && <button className=" btn btn-ui" onClick={handleClickNext}>{!isTheLastQuestion ? "Próxima" : "Finalizar"}</button>}
             </div>
           </>
